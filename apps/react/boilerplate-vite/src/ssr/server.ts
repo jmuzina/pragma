9b6aec9fb8a344171ef2ad046d138267ea003361
Julie Muzina <julie.muzina@canonical.com>
2025-01-16 17:52:56 -0500

    vercel configuration

--- apps/react/boilerplate-vite/src/ssr/server.ts
+++ apps/react/boilerplate-vite/src/ssr/server.ts
@@ -3,16 +3,23 @@ import { serveStream } from "@canonical/react-ssr/server";
 import express from "express";
 import render from "./renderer.js";
 
-const PORT = process.env.PORT || 5173;
+const NODE_ENV = process.env.NODE_ENV || "development";
 
 const app = express();
 
 app.use("/(assets|public)", express.static("dist/client/assets"));
 
-app.use(serveStream(render));
+export const GET = serveStream(render);
 
-app.listen(PORT, () => {
-  console.log(`Server started on http://localhost:${PORT}/`);
-});
+app.use(GET);
+
+// In development, directly listen with express.
+// In production, Vercel will serve the named `GET` export.
+if (NODE_ENV === "development") {
+  const PORT = process.env.PORT || 5173;
+  app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}/`);
+  });
+}
 
 export default app;
