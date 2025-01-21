418b8cbf800def84d5b611b52a28b5413a084722
Julie Muzina <julie.muzina@canonical.com>
2025-01-21 14:25:05 -0500

    simpler get handler

--- apps/react/boilerplate-vite/src/ssr/renderer.tsx
+++ apps/react/boilerplate-vite/src/ssr/renderer.tsx
@@ -6,4 +6,12 @@ const Renderer = new JSXRenderer(EntryServer, {
   htmlString,
 });
 
+//@ts-ignore
+export async function GET(req, res) {
+  // res.setHeader("Content-Type", "text/html");
+  // res.setHeader("Transfer-Encoding", "chunked");
+  // Renderer.render(req, res);
+  res.send("this is a test");
+}
+
 export default Renderer.render;
