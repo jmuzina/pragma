f2766ee2fb662962f9d3accbcfff874da573479b
Julie Muzina <julie.muzina@canonical.com>
2025-01-21 15:58:48 -0500

    adjust imports

--- apps/react/boilerplate-vite/.netlify/functions/api.js
+++ apps/react/boilerplate-vite/.netlify/functions/api.js
@@ -1,21 +1,11 @@
-// import serverless from "serverless-http";
-// import app from "../../api/server/server";
-// export const handler = serverless(app);
-
-import { serveStream } from "@canonical/react-ssr/server";
-import express, { Router } from "express";
 import serverless from "serverless-http";
-import render from "../../src/ssr/renderer.js";
+import app from "../../api/server/server";
 
-const api = express();
-const router = Router();
+app.get("/hello", (req, res) => res.send("Hello World!"));
 
-router.get("/hello", (req, res) => res.send("Hello World!"));
+// // tmp, refactor me to just serve the app itself
+// router.use("/ssr", serveStream(render));
+// api.use("/(assets|public)", express.static("api/client/assets"));
+//
 
-// tmp, refactor me to just serve the app itself
-router.use("/ssr", serveStream(render));
-api.use("/(assets|public)", express.static("api/client/assets"));
-
-api.use("/api/", router);
-
-export const handler = serverless(api);
+export const handler = serverless(app);
