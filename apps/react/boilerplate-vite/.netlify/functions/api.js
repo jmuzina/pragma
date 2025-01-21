34c32c4ec5656d44447bfc2b74f5321cc98007bf
Julie Muzina <julie.muzina@canonical.com>
2025-01-21 15:54:39 -0500

    try to serve our own app

--- apps/react/boilerplate-vite/.netlify/functions/api.js
+++ apps/react/boilerplate-vite/.netlify/functions/api.js
@@ -1,15 +1,20 @@
 // import serverless from "serverless-http";
 // import app from "../../api/server/server";
-//
 // export const handler = serverless(app);
 
+import { serveStream } from "@canonical/react-ssr/server";
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import render from "../../src/ssr/renderer.js";
 
 const api = express();
-
 const router = Router();
+
 router.get("/hello", (req, res) => res.send("Hello World!"));
+
+// tmp, refactor me to just serve the app itself
+router.use("/ssr", serveStream(render));
+api.use("/(assets|public)", express.static("api/client/assets"));
 
 api.use("/api/", router);
 
