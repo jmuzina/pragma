03d2bd412beeb1c5e38603f1af9b1deea88818ad
Julie Muzina <jmuzina@jmuzina.io>
2025-02-06 15:16:45 -0500

    chore(ci): Update initial changelog generation script with some better documentation

--- scripts/create-initial-changelogs.sh
+++ scripts/create-initial-changelogs.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
+# This script generates retroactive changelogs for a monorepo with Lerna and Conventional Commits.
+# It allows projects that are not currently using Conventional Commits to generate changelogs for their existing commit history.
+# This script is intended to be run once, just before the first release after adopting Conventional Commits versioning.
 # Follows https://github.com/lerna/lerna/tree/c3e601bee1bd6416e8b067ec8121abd996163d86/libs/commands/version#generating-initial-changelogs
-# TODO delete me after publishing initial changelogs
 
 # Clean dependencies and build artifacts
 bun run special:clean && rm -rf ./**/dist packages/generator-ds/generators
 
-# Clean-install and test
+# Clean-install and test before generating changelogs
+# These instructions are specific to the DS-25 project and can be removed or modified as needed.
 bun install --ignore-scripts
 bun run check
 bun run test
