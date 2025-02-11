27474b3e13d43260309cc6dcfbea25b10819c826
Julie Muzina <julie.muzina@canonical.com>
2025-02-11 16:38:40 -0500

    fix(storybook-baseline-grid): Fix storybook baseline grid addon storybook not running (#127)

--- packages/storybook-addon-baseline-grid/.storybook/local-preset.js
+++ packages/storybook-addon-baseline-grid/.storybook/local-preset.js
@@ -2,14 +2,14 @@
  * to load the built addon in this test Storybook
  */
 function previewAnnotations(entry = []) {
-    return [...entry, require.resolve("../dist/preview.js")];
-  }
+  return [...entry, require.resolve("../dist/esm/preview.js")];
+}
 
-  function managerEntries(entry = []) {
-    return [...entry, require.resolve("../dist/manager.js")];
-  }
+function managerEntries(entry = []) {
+  return [...entry, require.resolve("../dist/esm/manager.js")];
+}
 
-  module.exports = {
-    managerEntries,
-    previewAnnotations,
-  };
+module.exports = {
+  managerEntries,
+  previewAnnotations,
+};
