db1fb7693a48fe076ac11c52e1068845f457216e
advl <47156835+advl@users.noreply.github.com>
2025-03-21 15:25:12 +0100

    feat(boilerplate): Add storybook (#162)
    * Added storybook config and deps
    
    * Added storybook config to boilerplate
    
    * Added storybook config to boilerplate
    
    * Added storybook build files to gitignore
    
    * Added chromatic workflow for boilerplate
    
    ---------
    
    Co-authored-by: Julie Muzina <julie.muzina@canonical.com>

---
+++ apps/react/boilerplate-vite/.storybook/main.ts
@@ -0,0 +1,8 @@
+import { createConfig } from "@canonical/storybook-config";
+
+const config = createConfig();
+
+/* Otherwise leads to a TS error "CSF Parsing error: Expected 'ObjectExpression' but found 'CallExpression' instead in 'CallExpression'."
+ * https://github.com/storybookjs/storybook/issues/26778#issuecomment-2584041985
+ */
+export default { ...config };
