a7b2a20a9961990ace01cdfdbe9abc75e011052d
Julie Muzina <julie.muzina@canonical.com>
2025-04-16 14:55:55 -0400

    Move tanstack dev tools top top left to get out of way of example settings

--- apps/react/demo/src/routes/__root.tsx
+++ apps/react/demo/src/routes/__root.tsx
@@ -5,7 +5,7 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <TanStackRouterDevtools position={"top-right"} />
+      <TanStackRouterDevtools position={"top-left"} />
     </>
   ),
 });
