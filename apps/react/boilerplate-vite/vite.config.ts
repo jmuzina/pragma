3085dec14c2992dd8ceffc4066d69800cfbc45ce
Julie Muzina <jmuzina@jmuzina.io>
2025-06-05 21:29:52 -0400

    react router wip

--- apps/react/boilerplate-vite/vite.config.ts
+++ apps/react/boilerplate-vite/vite.config.ts
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from "@react-router/dev/vite";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [reactRouter(), tsconfigPaths(), react()],
 });
